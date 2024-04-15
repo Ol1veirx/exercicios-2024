37599ec3f880a6b9e196a8ed8c12f12850dd20b6
Oliveira <liedsonwork@hotmail.com>
2024-04-15 17:11:42 -0300

    feat: Adiciona sessão de discussões e resumo
    - Adiciona a sessão de discussões para permitir aos usuários interagirem sobre o tópico.
    - Implementa a exibição do resumo do tópico para fornecer uma visão geral do conteúdo.
    - Adiciona testes automatizados para garantir a funcionalidade correta das novas funcionalidades.

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevChuva';
+
+  isShowMore = false;
+
+  toggleShowMore() {
+    this.isShowMore = !this.isShowMore;
+  }
 }
